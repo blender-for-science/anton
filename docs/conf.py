7d34e2653052449fd1512b72d1f326cc4472c7eb
imsenthur <imsenthur@gmail.com>
2020-08-31 00:05:52 +0530

    Added extra nav links.

--- docs/conf.py
+++ docs/conf.py
@@ -47,6 +47,12 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 #
 html_theme = 'alabaster'
 
+html_theme_options = {
+        "extra_nav_links": {
+            "Source": "https://github.com/blender-for-science/anton",
+            "Releases": "https://github.com/blender-for-science/anton/releases",
+            }
+        }
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
